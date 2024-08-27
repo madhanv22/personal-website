e1b9caf6bc5293287900571bb3ccbb6c9059d547
madhanv22 <madhankumarv40@gmail.com>
2024-08-27 11:30:07 -0700

    duplicate files deleted

--- form.js
+++ form.js
@@ -1,12 +1,6 @@
-<<<<<<< HEAD
+
 window.onbeforeunload = () => {
   for(const form of document.getElementsByTagName('form')) {
     form.reset();
   }
-=======
-window.onbeforeunload = () => {
-  for(const form of document.getElementsByTagName('form')) {
-    form.reset();
-  }
->>>>>>> 00c8605cf089071456bfd8dc9526d58bcb9deba3
 }
--- script.js
+++ script.js
@@ -1,15 +1,6 @@
-<<<<<<< HEAD
 document.querySelector('.menu-toggle').addEventListener('click', function() {
   document.querySelector('.nav-links').classList.toggle('active');
 });
 
 
 
-=======
-document.querySelector('.menu-toggle').addEventListener('click', function() {
-  document.querySelector('.nav-links').classList.toggle('active');
-});
-
-
-
->>>>>>> 00c8605cf089071456bfd8dc9526d58bcb9deba3
